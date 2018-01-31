754a790f6ed25ce2946feb04df7d17d06def6f62
Lucas Cunha <Oliveira.lucasc93@hotmail.com>
2018-01-30 22:19:40 -0200

    Corrigido formulário

--- src/app/admin/screen-diagnostic/screen-diagnostic.component.ts
+++ src/app/admin/screen-diagnostic/screen-diagnostic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { AppService } from './../../app.service';
 import { AdminComponent } from './../admin.component';
